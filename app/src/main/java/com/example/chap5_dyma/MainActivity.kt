6a30680c90b728d5d7acbe3b8b9278159345f2b9
n3ominer <n3ominer@proton.me>
2024-08-19 16:40:52 +0200

    Update code for lesson 1

--- app/src/main/java/com/example/chap5_dyma/MainActivity.kt
+++ app/src/main/java/com/example/chap5_dyma/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.chap5_dyma
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var text: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        text = findViewById(R.id.text)
+
+        val spannable = SpannableString("Bold and Italic Text")
+
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        spannable.setSpan(StyleSpan(Typeface.ITALIC), 9, 15, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        text.text = spannable
+
+
+
     }
 }
